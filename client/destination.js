ad90a178080498044c371d486ae2182a5dc898de
EthanBagunas <epbagunas@gmail.com>
2023-08-02 19:55:53 +0800

    clean

--- client/destination.js
+++ client/destination.js
@@ -7,6 +7,7 @@ var code = ["12d", "12l", "12g", "03a", "02b", "01c", "10h", "44"]
 // var keys =Object.keys(Alllayers);  
 
 var count; // for the overlap method in scan
+var list;
 
 var showLayers={
 
@@ -17,47 +18,64 @@ var baseLayer = {
   "Available Routes" : osm
 }
 
- class Scan {
-
-  constructor (lng, lat){
-
-    this.lng = lng;
-    this.lat = lat;
-    
-  };
-
-  async scan1km(){
-        for (let i =0; i < route.geo.length; i++) {
-        count = i;
-        var check = this.overlap();
-        if(check){
-          showLayers[await showSearch(code[i])] = layers[i];
-      }
-    }
-    L.control.layers(baseLayer, showLayers).addTo(map);
-  }
-
-  overlap(){
-
-    var circle = turf.circle([this.lng , this.lat], 1, {units :'kilometers'});
-    var lstring = route.geo[count].features[0].geometry.coordinates;
-    var line = turf.lineString(lstring);
-    return turf.booleanIntersects(line, circle);
-
-  }
-}
-
 L.Control.geocoder({
   defaultMarkGeocode: false
 })
   .on('markgeocode', function(e) {
       var bbox = e.geocode.bbox; 
       var center = bbox.getCenter();
-      
+      setAllPropertiesToNull(showLayers);
       L.marker(center).addTo(map);
       const scan = new Scan(center.lng, center.lat);
-      scan.scan1km();
+      scan.check();
   })
   .addTo(map);
 
+  function setAllPropertiesToNull(obj) {
+    list=null;
+    Object.keys(obj).forEach(key => delete obj[key]);
+    console.log(obj);
+  }
  
+
+  class Scan {
+
+    constructor (lng, lat){
+  
+      this.lng = lng;
+      this.lat = lat;
+      
+    };
+  
+    async check(){
+      var obj= await this.scan1km()
+      if (Object.keys(obj).length >0) {
+        list =L.control.layers(baseLayer, showLayers).addTo(map);
+      } else {
+      
+      alert("No nearby routes 1km from the desctination !!!")
+      }
+    }
+  
+    async scan1km(){
+          for (let i =0; i < route.geo.length; i++) {
+          count = i;
+          var check = this.overlap();
+          if(check){
+            showLayers[await showSearch(code[i])] = layers[i];
+        }
+      }
+  
+      return showLayers
+    }
+  
+    overlap(){
+  
+      var circle = turf.circle([this.lng , this.lat], 1, {units :'kilometers'});
+      var lstring = route.geo[count].features[0].geometry.coordinates;
+      var line = turf.lineString(lstring);
+      return turf.booleanIntersects(line, circle);
+  
+    }
+  }
+  
